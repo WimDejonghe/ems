edef7993ebf44e21e384a83c007902a2b7774de0
Wim Dejonghe <wim.dejonghe@vives.be>
2023-08-18 11:54:36 +0200

    commit19

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -46,8 +46,7 @@ module.exports = {
           collapsable: false,
           children: [
             '',
-            '../esp32/index',
-                      
+            './esp32/index.md',       
           ]
         }
       ],
