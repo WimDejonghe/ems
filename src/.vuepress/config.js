ced5d453d9af93f9338b0c03ec2381eb43196bba
Wim Dejonghe <wim.dejonghe@vives.be>
2023-08-20 17:45:41 +0200

    commit39

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -47,6 +47,7 @@ module.exports = {
           children: [
             '',
             'intro/test',
+            'part1/test',
                 
           ]
         }
---
+++ src/guide/part1/test.md
@@ -0,0 +1,5 @@
+# Les2
+
+![example image](./images/vsc_instl.png "An exemplary image")
+
+src\guide\part1\images\vsc_instl.png
